ecb245aaf0670953648240c18f92eb65fcfa95e4
ravi0-web <ravi.01022004g@gmail.com>
2025-05-15 12:13:00 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from .views import recommend_phones
-
+from .views import recommend_phones,contact_view
 from .import views
 
 urlpatterns = [
@@ -19,6 +19,8 @@ path('phone/<slug:model_slug>/', views.phone_detail_view, name='phone_detail'),
         path('search-phones/', views.search_phones, name='search_phones'),  
     path('compare/', views.compare, name='compare'),
 
+     path('contact/', contact_view, name='contact'),
+     path('about/', views.about_view, name='about'),
 
 
 ]
