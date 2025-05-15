4c8ba2792bbcd89d7288ca89c85f51d14de80ad8
ravi0-web <ravi.01022004g@gmail.com>
2025-05-15 12:11:43 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -6,3 +6,12 @@ class User(models.Model):
     email=models.CharField(max_length=122)
     password=models.CharField(max_length=122)
     confirm_password=models.CharField(max_length=122)
+    
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
